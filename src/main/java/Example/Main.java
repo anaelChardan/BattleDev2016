43711f53bdf34b04c5432c04363d943ce424e7b8
Guillaume Chanson <guillaume.chns@gmail.com>
2016-11-08 20:08:57 +0100

    Premier exercice

--- src/main/java/Example/Main.java
+++ src/main/java/Example/Main.java
@@ -3,24 +3,31 @@ package Example;
 import Utils.Reader;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
-//EXEMPLE DU MOT LE PLUS LONG DANS
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.setIn(Reader.getInstance().getFile("EXEMPLE_1.txt"));
+        System.setIn(Reader.getInstance().getFile("input1.txt"));
 
+        String line;
+        Scanner sc = new Scanner(System.in);
 
-        Scanner scan = new Scanner(System.in);
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        int N = scan.nextInt();
-        while (scan.hasNext())
-        {
-            list.add(scan.next().length());
+        final int nbGants = sc.nextInt();
+        final Map<String, Integer> couleurs = new HashMap<>(nbGants);
+        while (sc.hasNextLine()) {
+            final String couleur = sc.nextLine();
+
+            if (couleurs.containsKey(couleur)) {
+                couleurs.put(couleur, couleurs.remove(couleur) + 1);
+            } else {
+                couleurs.put(couleur, 1);
+            }
         }
-        scan.close();
-        System.out.println(Collections.max(list));
+
+        int nbPaires = 0;
+        for (Integer nb : couleurs.values()) {
+            nbPaires += Math.floorDiv(nb, 2);
+        }
+        System.out.println(nbPaires);
     }
 }
