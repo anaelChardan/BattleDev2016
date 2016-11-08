e833b4be66dd0598e966ff9f1f877b538b4b6df7
anaelChardan <anael.chardan@gmail.com>
2016-11-08 15:22:59 +0100

    An exemple for tonight

--- src/main/java/Example/Main.java
+++ src/main/java/Example/Main.java
@@ -3,13 +3,24 @@ package Example;
 import Utils.Reader;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
+//EXEMPLE DU MOT LE PLUS LONG DANS
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.setIn(Reader.getInstance().getFile("Q1_1.txt"));
-        char ret = (char)System.in.read();
+        System.setIn(Reader.getInstance().getFile("EXEMPLE_1.txt"));
 
-        // returns the first character
-        System.out.println(ret);
+
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int N = scan.nextInt();
+        while (scan.hasNext())
+        {
+            list.add(scan.next().length());
+        }
+        scan.close();
+        System.out.println(Collections.max(list));
     }
 }
--- src/main/java/Q1/Main.java
+++ src/main/java/Q1/Main.java
@@ -1,16 +1,8 @@
 package Q1;
 
-import Utils.Reader;
-
-import java.io.FileInputStream;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.setIn(Reader.getInstance().getFile("Q1_1.txt"));
-        char ret = (char)System.in.read();
-
-        // returns the first character
-        System.out.println(ret);
     }
 }
